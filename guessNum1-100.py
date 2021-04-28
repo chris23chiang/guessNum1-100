f3e1743df92ce0f40fe5f0183d036c3138cf5208
Chris Chiang <chris23chiang@gmail.com>
2021-04-29 00:58:36 +0800

    add range and tell the range

--- guessNum1-100.py
+++ guessNum1-100.py
@@ -4,21 +4,23 @@
 #猜錯的話 要告訴他 比答案大/小
 
 import random
-ans = random.randint(1, 100)
+ans_min = eval(input('請決定要猜數字的最小值: '))
+ans_max = eval(input('請決定要猜數字的最大值: '))
+ans = random.randint(ans_min, ans_max)
 count = 0
 
 while True:
 	count += 1
 	print('這是你猜的第', count, "次!")
-	guess = eval(input('請輸入一個數字1~100:'))
+	guess = eval(input('請輸入一個數字{}~{}:'.format(ans_min, ans_max)))
 	if guess == ans:
 		print("終於猜對了!")
 		break
 	else:
 		if guess > ans:
-			print('比答案還大!')
 			print(' ')
+			ans_max = guess - 1
 		else:
-			print('比答案還小!')
 			print(' ')
+			ans_min = guess + 1
 
